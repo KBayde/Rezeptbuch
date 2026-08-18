0db40de40c27f3d0bb37adeb4c256b69b260fc88
KBayde <jonas.bejeuhr@gmx.de>
2026-08-18 12:20:26 +0200

    Fix: Syntaxfehler in utils.js (escapeHtml Quote-Escaping)

--- utils.js
+++ utils.js
@@ -18,7 +18,7 @@ export function escapeHtml(value) {
     .replaceAll("&", "&amp;")
     .replaceAll("<", "&lt;")
     .replaceAll(">", "&gt;")
-    .replaceAll(""", "&quot;")
+    .replaceAll('"', "&quot;")
     .replaceAll("'", "&#39;");
 }
 
