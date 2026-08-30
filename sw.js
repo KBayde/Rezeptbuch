7014922d1872ce0ba60999c5370fc6fce746c04d
KBayde <jonas.bejeuhr@gmx.de>
2026-08-30 19:28:57 +0200

    sw.js: Cache-Bump + neue Datei fuer Foto-Inventur

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Sehr einfacher Service Worker: cached nur die App-Huelle (HTML/CSS/JS),
 // damit die App auch bei wackligem Netz schnell startet. Rezeptdaten selbst
 // kommen immer live von Supabase (kein Offline-Datenzugriff in Phase 1).
-const CACHE_NAME = "clevulo-shell-v28";
+const CACHE_NAME = "clevulo-shell-v29";
 const SHELL_FILES = [
   "./",
   "./index.html",
@@ -22,6 +22,7 @@ const SHELL_FILES = [
   "./youtubeImport.js",
   "./linkImport.js",
   "./inventory.js",
+"./inventoryPhotoImport.js",
   "./costs.js",
     "./receiptImport.js",
   "./settings.js",
